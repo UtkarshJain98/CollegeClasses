3a7769047f94155c796dfc4d7b42fa5fee0302ce
Utkarsh Jain <utkjain@gmail.com>
2017-12-04 02:23:58 -0500

    init

---
+++ CollegeClasses/DotNetAppSqlDb/Models/TodoList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetAppSqlDb.Models
+{
+    public class TodoList
+    {
+        public int ID { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDetails { get; set; }
+        public string CourseProf { get; set; }
+        public bool privateCourse { get; set; }
+
+
+
+    }
+}
